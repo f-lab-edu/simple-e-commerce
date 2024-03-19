9989328352f62f551f286d17e294db1bfad87fa6
dev-jbkang <jbin8797@gmail.com>
2024-03-19 22:00:45 +0900

    feat: 할인률에 따른 UI 조건 처리

--- src/pages/home/components/ProductItem.tsx
+++ src/pages/home/components/ProductItem.tsx
@@ -1,6 +1,6 @@
 import { ShoppingBagIcon } from 'lucide-react';
 
-import { Product } from '../types';
+import type { Product } from '@/pages/home/types';
 import { numberWithCommas } from '@/lib/utils';
 
 import { Card, CardContent } from '@/components/ui/card';
@@ -10,22 +10,26 @@ interface ProductItemProps {
   item: Product;
 }
 
-export default function ProductItem({ item }: ProductItemProps) {
+export function ProductItem({ item }: ProductItemProps) {
   return (
     <Card className="border-0">
       <CardContent className="w-auto flex flex-col p-2.5 space-y-2">
         <div className="h-[210px] flex justify-center items-center">
           <img
-            className="max-w-full min-h-full rounded-md object-cover transition-all hover:scale-105"
+            className="w-full h-full rounded-md object-cover transition-all hover:scale-105"
             src={item.thumbnail}
             alt={`${item.productName}`}
           />
         </div>
         <div className="space-y-2">
           <div className="flex items-center space-x-1.5">
-            <div className="px-2 py-1 rounded-md bg-destructive text-xs text-white">{`${item.discount}%`}</div>
+            {item.discount > 0 && (
+              <div className="px-2 py-1 rounded-md bg-destructive text-xs text-white">{`${item.discount}%`}</div>
+            )}
             <p>{`${numberWithCommas(item.discountedPrice)}원`}</p>
-            <p className="text-sm text-outline line-through">{`${numberWithCommas(item.price)}원`}</p>
+            {item.discount > 0 && (
+              <p className="text-sm text-outline line-through">{`${numberWithCommas(item.price)}원`}</p>
+            )}
           </div>
           <p className="truncate text-sm font-semibold">{item.productName}</p>
           <p className="text-xs">{item.brandName}</p>
