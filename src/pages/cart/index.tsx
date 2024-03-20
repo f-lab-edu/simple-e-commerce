d3e0274549db4501537b4fec55ae1dbbdcceac09
dev-jbkang <jbin8797@gmail.com>
2024-03-20 21:56:05 +0900

    feat:장바구니 페이지 UI 구성

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,22 @@
+import { AllCheckbox } from './components/AllCheckbox';
+import { CartList } from '@/pages/cart/components/CartList';
+import { OrderInfo } from './components/OrderInfo';
+import { EmptyCart } from './components/EmptyCart';
+
 export default function CartPage() {
+  const data = [];
+
   return (
-    <div>cart</div>
-  )
+    <>
+      {data.length > 0 ? (
+        <>
+          <AllCheckbox />
+          <CartList />
+          <OrderInfo />
+        </>
+      ) : (
+        <EmptyCart />
+      )}
+    </>
+  );
 }
