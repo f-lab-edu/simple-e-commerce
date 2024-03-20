dcd813f912d6b39016f3c674445b78b9f813c263
dev-jbkang <jbin8797@gmail.com>
2024-03-20 15:14:15 +0900

    feat: 상태관리 스토어 구현

--- src/store/useProductStore.ts
+++ src/store/useProductStore.ts
@@ -3,10 +3,10 @@ import { create } from 'zustand';
 import type { Product } from '@/pages/home/types';
 import productsData from '@/mocks/data/products.json';
 
-interface StoreState {
+interface State {
   products: Product[];
 }
 
-export const useStore = create<StoreState>()(() => ({
+export const useProductStore = create<State>()(() => ({
   products: productsData as Product[],
 }));
