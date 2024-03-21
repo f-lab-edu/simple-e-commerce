f7e1e269e1a58a58dcfd4fc26ab8eec55c9470fc
dev-jbkang <jbin8797@gmail.com>
2024-03-21 21:49:46 +0900

    feat: 필터링 구현 & 렌더링 처리

--- src/pages/home/components/ProductList.tsx
+++ src/pages/home/components/ProductList.tsx
@@ -1,36 +1,41 @@
-import { Product } from '../types';
+import { useEffect } from 'react';
+
+import { Product, Category } from '@/pages/home/types';
+import { useProductStore } from '@/store/useProductStore';
 
 import ProductItem from './ProductItem';
 
-const PRODUCTS: Product[] = [
-  {
-    id: 0,
-    type: 'outer',
-    thumbnail: 'images/product1.png',
-    productName: '항공점퍼',
-    brandName: 'jump',
-    price: 50000,
-    discount: 10,
-    discountedPrice: 45000,
-  },
-  {
-    id: 1,
-    type: 'top',
-    thumbnail: 'images/product2.png',
-    productName: '브이 프린팅 검정 반팔티',
-    brandName: '브이',
-    price: 30000,
-    discount: 10,
-    discountedPrice: 27000,
-  },
-];
-
 export function ProductList() {
+  const { productsData, currentCategory, filteredProducts, setFilteredProducts } = useProductStore();
+  const products = currentCategory ? filteredProducts : productsData;
+
+  const getFilteredList = (products: Product[], category: Category) => {
+    const filteredList = products.filter((product) => product.type === category.value);
+    return filteredList;
+  };
+
+  useEffect(() => {
+    if (currentCategory) {
+      const filteredList = getFilteredList(productsData, currentCategory);
+      setFilteredProducts(filteredList);
+    }
+  }, [productsData, currentCategory, setFilteredProducts]);
+
   return (
-    <ul className="grid grid-cols-3 gap-4">
-      {PRODUCTS.map((item) => (
-        <ProductItem key={item.id} item={item} />
-      ))}
-    </ul>
+    <>
+      {products.length > 0 ? (
+        <ul className="grid grid-cols-3 gap-4">
+          <>
+            {products.map((item) => (
+              <ProductItem key={item.id} item={item} />
+            ))}
+          </>
+        </ul>
+      ) : (
+        <div className="pt-60">
+          <p className="text-center">상품이 존재하지 않습니다</p>
+        </div>
+      )}
+    </>
   );
 }
