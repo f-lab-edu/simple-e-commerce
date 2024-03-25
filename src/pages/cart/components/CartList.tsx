4b0b0d4546dc369c2bf763ea159515aacea709bc
dev-jbkang <jbin8797@gmail.com>
2024-03-25 14:30:31 +0900

    fix: 배경색상 default color로 변경

--- src/pages/cart/components/CartList.tsx
+++ src/pages/cart/components/CartList.tsx
@@ -2,7 +2,7 @@ import { CartItem } from '@/pages/cart/components/CartItem';
 
 export function CartList() {
   return (
-    <ul className="space-y-4 pt-0.5 pb-2.5 bg-[#f2f2f2]">
+    <ul className="space-y-4 pt-0.5 pb-2.5 bg-gray-100">
       {[...Array(8)].map((_, index) => (
         <CartItem key={index} />
       ))}
