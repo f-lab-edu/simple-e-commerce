5a97f8d866304cc4a5d39158d94de9e45345e2f5
dev-jbkang <jbin8797@gmail.com>
2024-03-27 21:38:04 +0900

    feat: 수량 및 체크상태 처리

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,16 +1,22 @@
 import { create } from 'zustand';
 
+import type { CartProduct } from '@/pages/cart/types';
 import type { Product } from '@/pages/home/types';
 
 interface State {
-  cartList: Product[];
+  cartList: CartProduct[];
 }
 
 interface Actions {
   addToCart: (item: Product) => void;
+  addQuantity: (id: number) => void;
 }
 
 export const useCartStore = create<State & Actions>()((set) => ({
   cartList: [],
-  addToCart: (item: Product) => set((state) => ({ cartList: [...state.cartList, item] })),
+  addToCart: (item) => set((state) => ({ cartList: [...state.cartList, { ...item, quantity: 1, checked: true }] })),
+  addQuantity: (id) =>
+    set((state) => ({
+      cartList: state.cartList.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)),
+    })),
 }));
