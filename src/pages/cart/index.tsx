d77b4715a1724f5fdaf180dc9a10ed167a3c2de4
dev-jbkang <jbin8797@gmail.com>
2024-03-28 13:55:53 +0900

    feat: 장바구니 목록 렌더링 처리

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,19 +1,21 @@
+import { useCartStore } from '@/store/useCartStore';
+
 import { AllCheckbox } from './components/AllCheckbox';
 import { CartList } from '@/pages/cart/components/CartList';
 import { OrderInfo } from './components/OrderInfo';
 import { EmptyCart } from './components/EmptyCart';
 
 export default function CartPage() {
-  const data = [];
+  const { cartList } = useCartStore();
 
   return (
     <>
-      {data.length > 0 ? (
-        <>
+      {cartList.length > 0 ? (
+        <div className={`min-h-[calc(100vh-64px)] flex flex-col`}>
           <AllCheckbox />
           <CartList />
           <OrderInfo />
-        </>
+        </div>
       ) : (
         <EmptyCart />
       )}
